1465a07273344ff06dbfdee9f805541caa54ef91
lixg <doingli@foxmail.com>
2025-01-11 16:14:18 +0800

    1

--- ethers/func.go
+++ ethers/func.go
@@ -1,9 +1,25 @@
 package ethers
 
 import (
+	"context"
 	"fmt"
+	"github.com/cobra-base/cobra-go/glog"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/ethclient"
 	"math/big"
+	"strings"
+	"time"
+)
+
+type TxStatus int
+
+const (
+	TxStatusUnknown  = TxStatus(0)
+	TxStatusSuccess  = TxStatus(1)
+	TxStatusFail     = TxStatus(2)
+	TxStatusPending  = TxStatus(3)
+	TxStatusNotFound = TxStatus(4)
 )
 
 func ParsePath(path []byte) ([]common.Address, []int, error) {
@@ -45,4 +61,64 @@ func BuildPath(addresses []common.Address, fees []int) ([]byte, error) {
 	}
 
 	return path, nil
+}
+
+func GetTxStatus(txHash string, endpoint string) TxStatus {
+	client, err := ethclient.Dial(endpoint)
+	if err != nil {
+		glog.Warnw("ethclient dail except", "endpoint", endpoint)
+		return TxStatusUnknown
+	}
+	defer client.Close()
+	return GetTxStatusWithClient(txHash, client)
+}
+
+func GetTxStatusWithClient(txHash string, client *ethclient.Client) TxStatus {
+	_, isPending, err := client.TransactionByHash(context.Background(), common.HexToHash(txHash))
+	if err != nil {
+		if strings.Contains(err.Error(), "not found") {
+			return TxStatusNotFound
+		}
+		glog.Warnw("transaction by hash except", "txHash", txHash, "err", err)
+		return TxStatusUnknown
+	}
+
+	if isPending {
+		return TxStatusPending
+	}
+
+	receipt, err := client.TransactionReceipt(context.Background(), common.HexToHash(txHash))
+	if err != nil {
+		if strings.Contains(err.Error(), "not found") {
+			return TxStatusNotFound
+		}
+		glog.Warnw("transaction receipt except", "txHash", txHash, "err", err)
+		return TxStatusUnknown
+	}
+
+	if receipt.Status == types.ReceiptStatusFailed {
+		return TxStatusFail
+	} else if receipt.Status == types.ReceiptStatusSuccessful {
+		return TxStatusSuccess
+	} else {
+		glog.Warnw("unrecognized transaction receipt status", "txHash", txHash, "status", receipt.Status)
+		return TxStatusUnknown
+	}
+}
+
+func WaitForTxStatus(txHash string, expired time.Duration, interval time.Duration, client *ethclient.Client) TxStatus {
+	status := TxStatusUnknown
+	startTime := time.Now()
+	for {
+		time.Sleep(interval)
+		txStatus := GetTxStatusWithClient(txHash, client)
+		if txStatus == TxStatusSuccess || txStatus == TxStatusFail {
+			status = txStatus
+			break
+		}
+		if time.Now().Sub(startTime).Milliseconds() > expired.Milliseconds() {
+			break
+		}
+	}
+	return status
 }
