a79c30e18b39a20aefc6e7f750fec3b383515a29
lixg <doingli@foxmail.com>
2025-02-08 21:37:55 +0800

    1

--- utils/func.go
+++ utils/func.go
@@ -2,8 +2,19 @@ package utils
 
 import (
 	"github.com/cobra-base/cobra-go/glog"
+	"math/rand"
 	"runtime/debug"
 )
+
+func GenerateStrId(n int) string {
+
+	letters := []rune("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
 
 func SafeGo(callable func()) {
 	go func() {
